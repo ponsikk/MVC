5b2a94bd87c825005150f1fca7400eea23937ec3
Ilya <ponsik98@mail.ru>
2024-12-02 11:46:26 +0300

    rework

--- src/main/java/web/rep/CarServiceRep.java
+++
@@ -1,7 +0,0 @@
-package web.rep;
-
-import java.util.List;
-
-public interface CarServiceRep {
-    List<String> getCarsWithLimit(Integer count);
-}
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,29 +1,7 @@
-package web.service;
+package web.rep;
 
-import org.springframework.stereotype.Service;
-import web.rep.CarServiceRep;
-
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class CarService implements CarServiceRep {
-
-    private final List<String> carList = new ArrayList<>();
-
-    public CarService() {
-        carList.add("BMW");
-        carList.add("Audi");
-        carList.add("Ford");
-        carList.add("Honda");
-        carList.add("Mercedes");
-    }
-    public List<String> getCarsWithLimit(Integer count) {
-        int limit = (count != null) ? count : Integer.MAX_VALUE;
-
-        if (count >= carList.size() || count <= 0) {
-            return carList;
-        }
-        return carList.subList(0, count);
-    }
+public interface CarServiceRep {
+    List<String> getCarsWithLimit(Integer count);
 }
